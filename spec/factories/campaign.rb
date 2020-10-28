492595e0efa0c8d5d13a8a9bd88a57cd35f4e023
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2020-10-28 20:12:35 -0300

    Add specs factories

---
+++ spec/factories/campaign.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :campaign do
+    title         { FFaker::Lorem.word }
+    description   { FFaker::Lorem.sentence }
+    user
+  end
+ end
+ 
---
+++ spec/factories/member.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :member do
+    name         { FFaker::Lorem.word }
+    email        { FFaker::Internet.email }
+    campaign
+  end
+ end
+ 
