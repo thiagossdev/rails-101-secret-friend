94666b89db45f83e3d5d2f2e0d81ea7b9cb39855
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2020-10-28 19:53:11 -0300

    Add mail-catcher

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("materialize-sprockets")
+require("jquery")
 
-import "../../assets/stylesheets/application"
+import 'materialize-css/dist/js/materialize'
+import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
